97698c16e335d232cc2e51766cbdbb363dd24399
moad6127 <moad6127@gmail.com>
2023-05-16 17:13:08 +0900

    Feat : C++추가하기
    C++ 로 Item클래스 추가하기

---
+++ Source/Slash/Private/Items/Item.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Item.h"
+
+// Sets default values
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
