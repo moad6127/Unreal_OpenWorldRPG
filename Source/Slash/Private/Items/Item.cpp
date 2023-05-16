310e3c4c9fd1e6c9f939f894b1d074da95567774
moad6127 <moad6127@gmail.com>
2023-05-16 19:05:00 +0900

    Feat : 유데미 강좌
    언리얼엔진의 디버그메시지 사용하기

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -15,11 +15,20 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 	
 	UE_LOG(LogTemp, Warning, TEXT("BeginPlayCalled"));
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Red, FString("Item OnScreen Message!"));
+	}
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	if (GEngine)
+	{
+		FString Message = FString::Printf(TEXT("DeltaTime : %f"), DeltaTime);
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Red, Message);
+	}
 }
 
