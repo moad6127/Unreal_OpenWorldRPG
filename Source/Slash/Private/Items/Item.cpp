42f2b260f6c1c4903f48754e81e27c186523a844
moad6127 <moad6127@gmail.com>
2023-05-18 19:05:12 +0900

    Feat : 유데미 강좌 : UFUNCTION속성
    UFunction속성을 사용해서 블루프린트의 이벤트 그래프에 C++함수를 노출시키기
    
    BlueprintCallable = 함수를 노출시키기
    BlueprintPure = 퓨어 함수로 만들어서 블루프린트에 노출시키기

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -18,6 +18,16 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 }
 
+float AItem::TransformedSin()
+{
+	return  Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return  Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
@@ -26,9 +36,9 @@ void AItem::Tick(float DeltaTime)
 
 	RunningTime += DeltaTime;
 
-	float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
+	//float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
 
-	AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
+	//AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
 
 	DRAW_SPHERE_SingleFrame(GetActorLocation());
 	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
