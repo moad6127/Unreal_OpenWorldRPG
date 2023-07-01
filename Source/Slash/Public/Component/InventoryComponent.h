05df9c3436783da595d972703e01676980aaa5d7
moad6127 <moad6127@gmail.com>
2023-07-01 21:21:11 +0900

    Feat : potion추가하기_1
    potion을 추가하기위해서 C++추가하기

--- Source/Slash/Public/Component/InventoryComponent.h
+++ Source/Slash/Public/Component/InventoryComponent.h
@@ -17,27 +17,22 @@ public:
 	UInventoryComponent();
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-
-	AItem* FindMatchingItem(AItem* ItemIn) const;
-	AItem* FindNextItemByID(AItem* ItemIn) const;
-	AItem* FindNextPartialStack(AItem* ItemIn)const;
-	void RemoveSingleInstanceOfItem(AItem ItemIn);
-	int32 RemoveAmountOfItem(AItem* ItemIn, int32 DesiredAmountToRemove);
-	void SplitExistingStack(AItem* ItemIn, const int32 AmountToSprit);
-
 protected:
 	virtual void BeginPlay() override;
+	void AddNewItem(AItem* Item, const int32 AmountToAdd);
 
 	float InventoryTotalWeight;
+	int32 InventorySlotsCapacity;
+	float InventoryWeightCapacity;
 
 	TArray<TObjectPtr<AItem>> InventoryContents;
 
 public:
-	//FORCEINLINE float GetInventoryTotalWeight() const {};
-	//FORCEINLINE float GetWeightCapacity() const {};
-	//FORCEINLINE int32 GetSlotCapacity() const {};
-	//FORCEINLINE TArray<AItem*> GetInventoryContents() const {};
+	FORCEINLINE float GetInventoryTotalWeight() const { return InventoryTotalWeight; }
+	FORCEINLINE float GetWeightCapacity() const { return InventoryWeightCapacity; }
+	FORCEINLINE int32 GetSlotCapacity() const { return InventorySlotsCapacity; }
+	FORCEINLINE TArray<AItem*> GetInventoryContents() const { return InventoryContents; }
 
-	//FORCEINLINE void SetSlotCapacity(const int32 NewSloCapacity) {};
-	//FORCEINLINE void SetWeightCapacity(const float NewWeightCapacity) {};
+	FORCEINLINE void SetSlotCapacity(const int32 NewSlotCapacity) { InventorySlotsCapacity = NewSlotCapacity; };
+	FORCEINLINE void SetWeightCapacity(const float NewWeightCapacity) { InventoryWeightCapacity = NewWeightCapacity; };
 };
---
+++ Source/Slash/Public/Items/Potion/PotionItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "PotionItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API APotionItem : public AItem
+{
+	GENERATED_BODY()
+	
+};
